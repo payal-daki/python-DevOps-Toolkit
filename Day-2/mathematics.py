8f846d2899956b3c06b66846c91ab1ecf8d24250
Payal Daki <130816609+payal-daki@users.noreply.github.com>
2024-07-10 11:28:01 +0530

    Create mathematics.py

---
+++ Day-2/mathematics.py
@@ -0,0 +1,15 @@
+import math
+
+value = 2.7
+
+# Square root
+sqrt_value = math.sqrt(value)
+print(f"Square Root: {sqrt_value}")  # Output: 1.6431676725154984
+
+# Exponential
+exp_value = math.exp(value)
+print(f"Exponential: {exp_value}")  # Output: 14.879731724872837
+
+# Logarithm
+log_value = math.log(value)
+print(f"Logarithm: {log_value}")  # Output: 0.9932517730102834
