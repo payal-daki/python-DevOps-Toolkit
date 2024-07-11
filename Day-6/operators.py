e780a7f3da1a37930a55455bb816980316dd0022
Payal Daki <130816609+payal-daki@users.noreply.github.com>
2024-07-11 10:54:10 +0530

    Create operators.py

---
+++ Day-6/operators.py
@@ -0,0 +1,48 @@
+#addition operator
+a = 5
+b = 3
+result = a + b
+print(result) 
+
+
+#assignment operator
+y = 10
+y += 3  
+print(y)
+
+
+#bitwise operator
+a= 5  # Binary: 0101
+b = 3  # Binary: 0011
+result = a & b  # Result: 0001 (Decimal: 1)
+print(result)
+
+#identity operator
+a = "hello"
+b = "world"
+result = a is not b
+print(result)
+
+#logical operator
+x = True
+y = False
+result = x and y
+print(result)
+
+#membership operator
+fruits = ["apple", "banana", "cherry"]
+result = "banana" in fruits
+print(result)
+
+#relational operator
+a = 5
+b = 5
+result = a == b
+print(result)
+
+#percedence operator
+result = 5 + 3 * 2
+print(result)
+# Multiplication has higher precedence, so result is 11, not 16
+
+
