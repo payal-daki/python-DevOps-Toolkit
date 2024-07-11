dc822860b65069123efcdc4553e68eff798a98b4
Payal Daki <130816609+payal-daki@users.noreply.github.com>
2024-07-11 18:29:11 +0530

    Create control.py

---
+++ Day-9/control.py
@@ -0,0 +1,15 @@
+#break statement
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    if number == 3:
+        break
+    print(number)
+
+
+#continue statement
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    if number == 3:
+        continue
+    print(number)
